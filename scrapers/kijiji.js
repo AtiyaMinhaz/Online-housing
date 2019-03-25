954e93a1b732467307676fa10b5ef8a82988862d
Andy Yeung <andyyeung87@hotmail.com>
2019-03-24 20:35:19 -0700

    add kijiji source

--- scrapers/kijiji.js
+++ scrapers/kijiji.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 const Housing = require('../models/Housing');
 const axios = require('axios');
 const cheerio = require('cheerio');
+require('dotenv').config({ path: '../.env' });
 
 /*
 Scraper is for educational purposes only:
 -limited to 1 HTTP request per 2 seconds
--only scrape the newest 120 (unique) posts
+-only scrape the newest 25 posts
 */
 
 mongoose
@@ -59,10 +60,11 @@ function getData() {
 }
 
 const SearchPage = {
+  url: 'https://www.kijiji.ca/b-for-rent/vancouver/c30349001l1700287?ad=offering',
   get() {
     return new Promise((resolve, reject) => {
       axios
-        .get('https://vancouver.craigslist.org/search/apa?bundleDuplicates=1')
+        .get(this.url)
         .then(res => {
           resolve(this.parseLinks(res.data));
         })
@@ -74,14 +76,15 @@ const SearchPage = {
   parseLinks(html) {
     const $ = cheerio.load(html);
     let links = [];
-    $('#sortable-results .rows')
+    $('#mainPageContent .container-results')
       .children()
       .each((i, elem) => {
-        links.push(
-          $(elem)
-            .children('a')
-            .attr('href')
-        );
+        if ($(elem).attr('data-ad-id')) {
+          let path = $(elem)
+            .find('.title a')
+            .attr('href');
+          links.push('https://www.kijiji.ca' + path);
+        }
       });
     return links;
   }
@@ -95,13 +98,27 @@ const ContentPage = {
         axios
           .get(link)
           .then(res => {
+            let data = {};
             try {
-              let data = this.parseDetails(res.data, link);
-              resolve(data);
+              data = this.parseDetails(res.data, link);
             } catch (e) {
-              console.log('skipping ' + link);
-              resolve();
+              console.log(`skipping ${link} :: ${e.message}`);
+              return resolve();
             }
+            this.convertAddressToLatLon(data.address)
+              .then(coords => {
+                if (coords.lat && coords.lon) {
+                  data.lat = coords.lat;
+                  data.lon = coords.lon;
+                  resolve(data);
+                } else {
+                  console.log(`skipping ${link} :: undefined lat-lon`);
+                  resolve();
+                }
+              })
+              .catch(err => {
+                reject(err);
+              });
           })
           .catch(err => {
             reject(err);
@@ -111,23 +128,19 @@ const ContentPage = {
   },
   parseDetails(html, link) {
     let $ = cheerio.load(html);
-    $ = cheerio.load($('.page-container .body').html());
 
-    let raw_title = $('#titletextonly').html();
-    let raw_posting_date = $('#display-date time').attr('datetime');
-    let raw_lat = $('#map').attr('data-latitude');
-    let raw_lon = $('#map').attr('data-longitude');
-    let raw_price = $('.postingtitle .price')
-      .html()
-      .substr(1);
-    let raw_bedrooms = $('.mapAndAttrs .shared-line-bubble:first-child b:nth-child(1)')
+    let raw_title = $('.title-2323565163').html();
+    let raw_posting_date = $('.datePosted-319944123 time').attr('datetime');
+    let raw_address = $('.address-3617944557').html();
+    let raw_price = $('.currentPrice-441857624 span').attr('content');
+    let raw_bedrooms = $('.itemAttributeWrapper-37588635:nth-child(1) .attributeValue-2574930263')
       .html()
       .charAt(0);
-    let raw_bathrooms = $('.mapAndAttrs .shared-line-bubble:first-child b:nth-child(2)')
+    let raw_bathrooms = $('.itemAttributeWrapper-37588635:nth-child(2) .attributeValue-2574930263')
       .html()
       .charAt(0);
 
-    if (!raw_title || !raw_posting_date || !raw_lat || !raw_lon || !raw_price || !raw_bedrooms || !raw_bathrooms) {
+    if (!raw_title || !raw_posting_date || !raw_address || !raw_price || !raw_bedrooms || !raw_bathrooms) {
       throw 'missing required field';
     } else if (isNaN(raw_price) || isNaN(raw_bedrooms) || isNaN(raw_bathrooms)) {
       throw 'type error';
@@ -136,20 +149,29 @@ const ContentPage = {
     return {
       title: raw_title,
       type: 'rental',
-      lat: raw_lat,
-      lon: raw_lon,
+      address: raw_address,
       price: parseInt(raw_price),
       bedrooms: parseInt(raw_bedrooms),
       bathrooms: parseInt(raw_bathrooms),
       posting_date: new Date(raw_posting_date),
-      source: 'craigslist',
+      source: 'kijiji',
       link,
-      post_id: `cr${
-        link
-          .split('/')
-          .reverse()[0]
-          .split('.')[0]
-      }`
+      post_id: `ki${link.split('/').reverse()[0]}`
     };
+  },
+  // https://locationiq.com/docs#forward-geocoding
+  convertAddressToLatLon(address) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(`https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATIONIQ_TOKEN}&q=${address}&format=json`)
+        .then(res => {
+          let lat = res.data[0].lat;
+          let lon = res.data[0].lon;
+          resolve({ lat, lon });
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
   }
 };
